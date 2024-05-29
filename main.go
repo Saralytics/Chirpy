7d42fbfa6cab60e11e71abcdd9618ae69f5200b7
Saralytics <zhaoxue.li@outlook.com>
2024-05-29 08:06:25 +0400

    added miiddleware

--- main.go
+++ main.go
@@ -1,13 +1,62 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
 
+type apiConfig struct {
+	fileserverHits int
+}
+
+const metricsTemplate = `<html>
+
+<body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+</body>
+
+</html>
+`
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+
+	newHandler := func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(newHandler)
+}
+
+func (cfg *apiConfig) metricsHandler(w http.ResponseWriter, r *http.Request) {
+	metrics := fmt.Sprintf(metricsTemplate, cfg.fileserverHits)
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(metrics))
+}
+
+func (cfg *apiConfig) resetHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits = 0
+}
+
 func main() {
+	apiCfg := &apiConfig{}
+
+	checkHealthHandler := func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fileServerHandler := http.FileServer(http.Dir("."))
+	wrappedFileServerHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", fileServerHandler))
+	mux.Handle("/app/", wrappedFileServerHandler)
+	mux.HandleFunc("GET /api/healthz", checkHealthHandler)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
+	mux.HandleFunc("/api/reset", apiCfg.resetHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
@@ -15,4 +64,5 @@ func main() {
 	}
 	log.Println("Listening...")
 	log.Fatal(server.ListenAndServe())
+
 }
